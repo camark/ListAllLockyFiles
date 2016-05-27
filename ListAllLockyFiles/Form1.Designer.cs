0965ed615acfd6eff8399cdac7e6f34adf65c7e1
Vianney RUY <vruy@eu-symtrax.com>
2016-05-27 11:02:55 +0200

    save

---
+++ ListAllLockyFiles/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace ListAllLockyFiles
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.lvPath = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblComputer = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(26, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.lvPath});
+            this.listView1.FullRowSelect = true;
+            this.listView1.Location = new System.Drawing.Point(26, 41);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(554, 261);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listView1_MouseDoubleClick);
+            // 
+            // lvPath
+            // 
+            this.lvPath.Width = 542;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(505, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(194, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Computer:";
+            // 
+            // lblComputer
+            // 
+            this.lblComputer.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.lblComputer.AutoSize = true;
+            this.lblComputer.Location = new System.Drawing.Point(256, 21);
+            this.lblComputer.Name = "lblComputer";
+            this.lblComputer.Size = new System.Drawing.Size(59, 13);
+            this.lblComputer.TabIndex = 4;
+            this.lblComputer.Text = "Not started";
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button3.Location = new System.Drawing.Point(26, 308);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(554, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Export to Excel";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(592, 344);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.lblComputer);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "List all locky files on network";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader lvPath;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblComputer;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ ListAllLockyFiles/Form1.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+
+
+namespace ListAllLockyFiles
+{
+    public partial class Form1 : Form
+    {
+        private ArrayList lstNetComputers;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        delegate void SetTextCallback(string text);
+
+        private void SetText(string text)
+        {
+            // InvokeRequired required compares the thread ID of the
+            // calling thread to the thread ID of the creating thread.
+            // If these threads are different, it returns true.
+            if (this.lblComputer.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(SetText);
+                this.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                this.lblComputer.Text = text;
+            }
+        }
+
+        delegate void RefreshLabelCallback();
+
+        private void RefreshLabel()
+        {
+            // InvokeRequired required compares the thread ID of the
+            // calling thread to the thread ID of the creating thread.
+            // If these threads are different, it returns true.
+            if (this.lblComputer.InvokeRequired)
+            {
+                RefreshLabelCallback d = new RefreshLabelCallback(RefreshLabel);
+                this.Invoke(d, new object[] { });
+            }
+            else
+            {
+                this.lblComputer.Refresh();
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            _worker = new BackgroundWorker { WorkerSupportsCancellation = true };
+
+            _worker.DoWork += new DoWorkEventHandler((state, args) =>
+            {
+                ListAllServers();
+
+                foreach (string sComputer in lstNetComputers)
+                {
+                    SetText(sComputer);
+                    RefreshLabel();
+                    if (_worker.CancellationPending)
+                        break;
+                    ArrayList lstShares = ListShares((string)sComputer);
+
+                    ListAllLocky(lstShares);
+                }
+
+                button1.Enabled = true;
+                button2.Enabled = false;
+                _worker.CancelAsync();
+                lblComputer.Text = "Not started";
+
+            });
+
+            _worker.RunWorkerAsync();
+            button1.Enabled = false;
+            button2.Enabled = true;
+
+
+        }
+        private delegate void AddItemCallback(object o);
+
+        private void AddItem(object oItem)
+        {
+            if (this.listView1.InvokeRequired)
+            {
+                AddItemCallback d = new AddItemCallback(AddItem);
+                this.Invoke(d, new object[] { oItem });
+            }
+            else
+            {
+                if (oItem is string)
+                    listView1.Items.Add((string)oItem);
+            }
+        }
+
+        private delegate void EnsureVisibleCallback(object o);
+
+        private void EnsureVisible(object o)
+        {
+            if (this.listView1.InvokeRequired)
+            {
+                EnsureVisibleCallback d = new EnsureVisibleCallback(AddItem);
+                this.Invoke(d, new object[] { o });
+            }
+            else
+            {
+                listView1.Items[(int)o].EnsureVisible();
+            }
+        }
+
+        delegate void RefreshListCallback();
+
+        private void RefreshList()
+        {
+            // InvokeRequired required compares the thread ID of the
+            // calling thread to the thread ID of the creating thread.
+            // If these threads are different, it returns true.
+            if (this.lblComputer.InvokeRequired)
+            {
+                RefreshListCallback d = new RefreshListCallback(RefreshList);
+                this.Invoke(d, new object[] { });
+            }
+            else
+            {
+                this.listView1.Refresh();
+            }
+        }
+        private void ListAllLocky(ArrayList lstShares)
+        {
+            foreach (string oFolderPath in lstShares)
+            {
+                if (_worker.CancellationPending)
+                    break;
+                String[] allfiles;
+                try
+                {
+
+
+                    allfiles = System.IO.Directory.GetFiles(oFolderPath, "*._locky",
+                        System.IO.SearchOption.AllDirectories);
+                }
+                catch (Exception)
+                {
+
+                    return;
+                }
+                foreach (string sFile in allfiles)
+                {
+                    if (_worker.CancellationPending)
+                        break;
+                    AddItem(sFile);
+
+                    EnsureVisible(listView1.Items.Count - 1);
+                    RefreshList();
+                }
+            }
+        }
+
+        private void ListAllServers()
+        {
+            if (_worker.CancellationPending)
+                return;
+            NetworkBrowser oBrowser = new NetworkBrowser();
+            lstNetComputers = oBrowser.getNetworkComputers();
+        }
+
+        private ArrayList ListShares(string server)
+        {
+
+            ArrayList sSharesList = new ArrayList();
+
+            if (_worker.CancellationPending)
+                return sSharesList;
+            //// Enumerate shares on local computer
+            //Console.WriteLine("\nShares on local computer:");
+            ShareCollection shi = ShareCollection.LocalShares;
+
+            // Enumerate shares on a remote computer
+
+            if (server != null && server.Trim().Length > 0)
+            {
+                Console.WriteLine("\nShares on {0}:", server);
+                shi = ShareCollection.GetShares(server);
+                if (shi != null)
+                {
+                    foreach (Share si in shi)
+                    {
+                        if (_worker.CancellationPending)
+                            break;
+                        if ((si.Root == null) || si.Root.FullName.Contains("$"))
+                            continue;
+
+                        sSharesList.Add(si.Root.FullName);
+                    }
+                }
+                else
+                    Console.WriteLine("Unable to enumerate the shares on {0}.\n"
+                        + "Make sure the machine exists, and that you have permission to access it.",
+                        server);
+
+                Console.WriteLine();
+            }
+
+            return sSharesList;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = true;
+            button2.Enabled = false;
+            _worker.CancelAsync();
+            lblComputer.Text = "Not started";
+        }
+
+        public BackgroundWorker _worker { get; set; }
+
+        private void listView1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            String sPath = listView1.SelectedItems[0].Text;
+
+            string sDir = Path.GetDirectoryName(sPath);
+
+            if (sDir != null) Process.Start(sDir);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Microsoft.Office.Interop.Excel.Application xla = new Microsoft.Office.Interop.Excel.Application();
+
+            xla.Visible = true;
+
+            Microsoft.Office.Interop.Excel.Workbook wb = xla.Workbooks.Add(Microsoft.Office.Interop.Excel.XlSheetType.xlWorksheet);
+
+            Microsoft.Office.Interop.Excel.Worksheet ws = (Microsoft.Office.Interop.Excel.Worksheet)xla.ActiveSheet;
+
+            int i = 1;
+
+            int j = 1;
+
+            foreach (ListViewItem comp in listView1.Items)
+            {
+
+                ws.Cells[i, j] = comp.Text.ToString();
+
+                //MessageBox.Show(comp.Text.ToString());
+
+                foreach (ListViewItem.ListViewSubItem drv in comp.SubItems)
+                {
+
+                    ws.Cells[i, j] = drv.Text.ToString();
+
+                    j++;
+
+                }
+
+                j = 1;
+
+                i++;
+
+            }
+        }
+    }
+}
+
