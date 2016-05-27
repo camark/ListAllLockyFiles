df826a8ee75ea6759c19664d8ac47b7aeed31824
Vianney RUY <vruy@eu-symtrax.com>
2016-05-27 16:28:24 +0200

    dock button Excel

--- ListAllLockyFiles/Form1.Designer.cs
+++ ListAllLockyFiles/Form1.Designer.cs
@@ -100,7 +100,8 @@
             // 
             // button3
             // 
-            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.button3.Location = new System.Drawing.Point(26, 308);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(554, 23);
